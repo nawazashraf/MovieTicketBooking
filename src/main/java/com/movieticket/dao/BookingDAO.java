5befa2d085411a9fbcf3c3ac2179eb2ae18804a0
nawazashraf <ashrafnawaz843@gmail.com>
2026-08-26 18:06:40 +0530

    add booking confirmation

--- src/main/java/com/movieticket/dao/BookingDAO.java
+++ src/main/java/com/movieticket/dao/BookingDAO.java
@@ -46,4 +46,28 @@ public class BookingDAO {
 
 		return null;
 	}
+	
+	public boolean confirmBooking(String bookingId) {
+		String sql = """
+				UPDATE bookings
+				SET booking_status = 'CONFIRMED'
+				WHERE id = ?
+				""";
+		
+		try {
+			Connection conn = DBConnection.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			ps.setString(1, bookingId);
+			
+			return ps.executeUpdate() > 0;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+		
+	}
+	
 }

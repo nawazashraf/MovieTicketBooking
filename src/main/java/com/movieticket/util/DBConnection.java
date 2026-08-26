d491cad8541289ed7cc4e955a867a1c1bbe7e3fc
nawazashraf <ashrafnawaz843@gmail.com>
2026-08-26 14:30:14 +0530

    added env for db

--- src/main/java/com/movieticket/util/DBConnection.java
+++ src/main/java/com/movieticket/util/DBConnection.java
@@ -1,5 +1,25 @@
 package com.movieticket.util;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBConnection {
+	public static Connection getConnection() {
+		try {
+			String URL = "jdbc:mysql://localhost:3306/movie_ticket_booking";
+
+			String USER = "root";
+
+			String PASSWORD = "your_mysql_password";
+
+			String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+			Class.forName(DRIVER);
+
+			return DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }

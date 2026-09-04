c3d4c6e340d3f83879fb4818cb6a857130766cb8
iKhurshidansari <mdansari211203@gmail.com>
2026-09-04 19:15:04 +0530

    Update ShowBean.java

--- src/main/java/com/movieticket/model/ShowBean.java
+++ src/main/java/com/movieticket/model/ShowBean.java
@@ -1,51 +1,99 @@
 package com.movieticket.model;
 
+import java.sql.Timestamp;
+
 public class ShowBean {
 
-	private String showId;
-	private String movieName;
-	private String mallName;
-	private String showDate;
-	private String startTime;
-	
+    private String showId;
+    private String movieId;
+    private String mallId;
 
-	public String getShowId() {
-		return showId;
-	}
+    private String movieName;
+    private String mallName;
 
-	public void setShowId(String showId) {
-		this.showId = showId;
-	}
+    private String showDate;
+    private String startTime;
+    private String endTime;
+    private String status;
+    private Timestamp createdAt;
 
-	public String getMovieName() {
-		return movieName;
-	}
+    public String getShowId() {
+        return showId;
+    }
 
-	public void setMovieName(String movieName) {
-		this.movieName = movieName;
-	}
+    public void setShowId(String showId) {
+        this.showId = showId;
+    }
 
-	public String getMallName() {
-		return mallName;
-	}
+    public String getMovieId() {
+        return movieId;
+    }
 
-	public void setMallName(String mallName) {
-		this.mallName = mallName;
-	}
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
 
-	public String getShowDate() {
-		return showDate;
-	}
+    public String getMallId() {
+        return mallId;
+    }
 
-	public void setShowDate(String showDate) {
-		this.showDate = showDate;
-	}
+    public void setMallId(String mallId) {
+        this.mallId = mallId;
+    }
 
-	public String getStartTime() {
-		return startTime;
-	}
+    public String getMovieName() {
+        return movieName;
+    }
 
-	public void setStartTime(String startTime) {
-		this.startTime = startTime;
-	}
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMallName() {
+        return mallName;
+    }
+
+    public void setMallName(String mallName) {
+        this.mallName = mallName;
+    }
+
+    public String getShowDate() {
+        return showDate;
+    }
+
+    public void setShowDate(String showDate) {
+        this.showDate = showDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }

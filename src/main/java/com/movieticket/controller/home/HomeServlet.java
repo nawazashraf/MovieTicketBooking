a92dc5fe771241fb4011ddaf19717ae69e6d385b
nawazashraf <ashrafnawaz843@gmail.com>
2026-09-12 11:10:21 +0530

    Create HomeServlet.java

---
+++ src/main/java/com/movieticket/controller/home/HomeServlet.java
@@ -0,0 +1,28 @@
+package com.movieticket.controller.home;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import com.movieticket.dao.MovieDAO;
+import com.movieticket.model.MovieBean;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private final MovieDAO movieDAO = new MovieDAO();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		List<MovieBean> movies = movieDAO.getAllMovies();
+		request.setAttribute("movies", movies);
+
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+	}
+
+}
